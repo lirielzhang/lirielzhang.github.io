6001682f66c20e18a8ac8219fddc64bcc81b1776
lirielzhang <lirielzhang07@gmail.com>
2026-06-18 02:01:18 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const current = location.pathname.split('/').pop() || 'index.html';
-document.querySelectorAll('.nav a').forEach((link) => {
-  if (link.getAttribute('href') === current) link.classList.add('is-active');
-});
